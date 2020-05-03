9b8bd8897e1221bb155e20b3a1e6e16613f4fc8e
Andriy Andruhovski <andriy.andruhovski@aspose.com>
2020-05-03 23:03:49 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Aspose.HTML Converter
+
+Aspose.HTML Converter is built with [Aspose.HTML Cloud](https://products.aspose.cloud/html/family)
+It allows converting Markdown to HTML and PDF format.
+
+## Features
+
+Converter supports the following commands:
+
+* Convert MD to PDF
+* Convert MD to HTML
+* Export Converter settings
+
+By default, all actions run in the current directory.
+
+## Requirements
+
+We use the `node-fetch` module to makes [API calls](https://docs.aspose.cloud/display/htmlcloud/).
+
+## Usage
+
+### Command Palette
+
+1. Open the Markdown file
+1. Press `Ctrl+Shift+P`
+1. Type `Aspose:` and select below
+   * `Aspose: Export Converter settings)`
+   * `Aspose: Convert MD to PDF`
+   * `Aspose: Convert MD to HTML`
+
+![usage1](images/usage1.gif)
+
+### Menu
+
+1. Open the Markdown file
+1. Right click and select below
+   * `Aspose: Convert MD to PDF`
+   * `Aspose: Convert MD to HTML`
+
+![usage2](images/usage2.gif)
+
+## Extension Settings
+
+Markdown Converter contributes the following settings:
+
+* `outputDirectory`: Location for output files. Default is `.` (current).
+* `paper.orientation`: Paper orientation for PDF files. Accepted values `Portrait` or `Landscape`.
+* `paper.size`: Paper Size [`A0`, `A1`, `A2`, `A3`, `A4`, `A5`, `A6`, `Letter`, `Legal`]. The value `Custom` means using options `Width` and `Height` instead Paper Size.
+* `paper.width`: Paper Width, accepts values labeled with units(mm, cm, in, px).
+* `paper.height`: Paper Height, accepts values labeled with units(mm, cm, in, px).
+* `margin.top`: Top margin (units: mm, cm, in, px).
+* `margin.bottom`: Bottom margin (units: mm, cm, in, px).
+* `margin.right`: Right margin (units: mm, cm, in, px).
+* `margin.left`: Left margin (units: mm, cm, in, px)."
+
+## Known Issues
+
+* MD-to-PDF conversion: The `@page` CSS at-rule can be applied incorrectly.
+
+## Release Notes
+
+### 0.0.1
+
+Initial release of Aspose.HTML Markdown Converter
---
+++ spec/aspose-html-converter-spec.js
@@ -0,0 +1,73 @@
+'use babel';
+
+import AsposeHtmlConverter from '../lib/aspose-html-converter';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('AsposeHtmlConverter', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('aspose-html-converter');
+  });
+
+  describe('when the aspose-html-converter:toggle event is triggered', () => {
+    it('hides and shows the modal panel', () => {
+      // Before the activation event the view is not on the DOM, and no panel
+      // has been created
+      expect(workspaceElement.querySelector('.aspose-html-converter')).not.toExist();
+
+      // This is an activation event, triggering it will cause the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'aspose-html-converter:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        expect(workspaceElement.querySelector('.aspose-html-converter')).toExist();
+
+        let asposeHtmlConverterElement = workspaceElement.querySelector('.aspose-html-converter');
+        expect(asposeHtmlConverterElement).toExist();
+
+        let asposeHtmlConverterPanel = atom.workspace.panelForItem(asposeHtmlConverterElement);
+        expect(asposeHtmlConverterPanel.isVisible()).toBe(true);
+        atom.commands.dispatch(workspaceElement, 'aspose-html-converter:toggle');
+        expect(asposeHtmlConverterPanel.isVisible()).toBe(false);
+      });
+    });
+
+    it('hides and shows the view', () => {
+      // This test shows you an integration test testing at the view level.
+
+      // Attaching the workspaceElement to the DOM is required to allow the
+      // `toBeVisible()` matchers to work. Anything testing visibility or focus
+      // requires that the workspaceElement is on the DOM. Tests that attach the
+      // workspaceElement to the DOM are generally slower than those off DOM.
+      jasmine.attachToDOM(workspaceElement);
+
+      expect(workspaceElement.querySelector('.aspose-html-converter')).not.toExist();
+
+      // This is an activation event, triggering it causes the package to be
+      // activated.
+      atom.commands.dispatch(workspaceElement, 'aspose-html-converter:toggle');
+
+      waitsForPromise(() => {
+        return activationPromise;
+      });
+
+      runs(() => {
+        // Now we can test for view visibility
+        let asposeHtmlConverterElement = workspaceElement.querySelector('.aspose-html-converter');
+        expect(asposeHtmlConverterElement).toBeVisible();
+        atom.commands.dispatch(workspaceElement, 'aspose-html-converter:toggle');
+        expect(asposeHtmlConverterElement).not.toBeVisible();
+      });
+    });
+  });
+});
